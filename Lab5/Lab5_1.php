9dcb35c3cfcdff0e0003468dfe2ae28ab4c6c81f
ValenKek <57965516+ValenKek@users.noreply.github.com>
2021-12-05 01:08:59 +0200

    123

---
+++ Lab5/Lab5_1.php
@@ -0,0 +1,27 @@
+<?php
+$products = array("Alpha" => 55, "Beta" => 83, "Gamma"=> 41);
+
+echo CurrencyConverter($products);
+
+function CurrencyConverter ($products)
+{
+    $euro=30;
+    $dollar=27;
+
+    $productsKeys = array_keys($products);
+    $productStartPrice = array_values($products);
+
+    $productDollarPrice = array();
+    $productEuroPrice = array();
+
+    $result="<table border=1> ";
+
+    for($i = 0, $count = count($productsKeys); $i < $count;$i++){
+        $productDollarPrice[$i] =round($productStartPrice[$i] / $dollar, 3);
+        $productEuroPrice[$i] =round($productStartPrice[$i] / $euro, 3);
+
+        $result .= "<tr> <td> $productsKeys[$i] </td> <td>$productStartPrice[$i] uan </td> <td>$productDollarPrice[$i] usd </td> <td>$productEuroPrice[$i] eur </td> </tr>";
+    }
+    $result .=" </table>";
+    return $result;
+}
---
+++ Lab5/Lab5_2.php
@@ -0,0 +1,37 @@
+<?php
+
+function isEmailValid($email){
+    if(!isEmailHasCommercialAt($email)){
+        echo "Email should contain @ symbol <br>";
+    }
+    if(isEmailHasMoreThanOneCommercialAt($email)){
+        echo  "Email should contain only one @ symbol <br>";
+    }
+    if(!isEmailHaveDot($email)){
+        echo  "Email should contain dots <br>";
+    }
+    if(isEmailHaveSpace($email)){
+        echo  "Email shouldn`t contain spaces <br>";
+    }
+}
+function isEmailHasCommercialAt($email){
+    return strpos($email, '@') == true;
+}
+function isEmailHasMoreThanOneCommercialAt($email){
+    return substr_count($email, '@') > 1;
+}
+function isEmailHaveDot($email){
+    return strpos($email, '.') == true;
+}
+
+function isEmailHaveSpace($email){
+    return strpos($email, ' ') == true;
+}
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    isEmailValid($_POST['email']);
+}
+?>
+<form action="" method="post">
+    <input type="text" name ="email">
+    <input type="submit" name = "send" value="check email">
+</form>
